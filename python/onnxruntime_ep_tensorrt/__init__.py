a203ed482c53739ccef824759fafbaab260eb7f7
Chi Lo <Chi.Lo@microsoft.com>
2026-02-11 14:12:22 -0800

    add example usage

--- python/onnxruntime_ep_tensorrt/__init__.py
+++ python/onnxruntime_ep_tensorrt/__init__.py
@@ -1,5 +1,23 @@
 from __future__ import annotations
 import pathlib
+import os
+import ctypes
+import onnxruntime as ort
+
+ort_dir = os.path.dirname(os.path.abspath(ort.__file__))
+dll_path = os.path.join(ort_dir, "capi", "onnxruntime.dll")
+
+# When the application calls ort.register_execution_provider_library() with the path to the plugin EP DLL,
+# ORT internally uses LoadLibraryExW() to load that DLL. Since the plugin EP depends on onnxruntime.dll,
+# the operating system will attempt to locate and load onnxruntime.dll first.
+#
+# On Windows, LoadLibraryExW() searches the directory containing the plugin EP DLL before searching system directories.
+# Because onnxruntime.dll is not located in the plugin EP’s directory, Windows ends up loading the copy from a 
+# system directory instead — which is not the correct version.
+#
+# To ensure the plugin EP uses the correct onnxruntime.dll bundled with the ONNX Runtime package, 
+# we load that DLL explicitly before loading the plugin EP DLL.
+ctypes.WinDLL(dll_path)
 
 __all__ = ['get_library_path', 'get_ep_name', 'get_ep_names']
 
